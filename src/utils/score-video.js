bc02d5a40c2db36e732e538e28c42a8f867cebe0
Benjamin Lacroix <benjlacroix@gmail.com>
2017-02-23 21:07:20 +0100

    new input format

--- src/utils/score-video.js
+++ src/utils/score-video.js
@@ -2,19 +2,18 @@ const _ = require('lodash');
 
 module.exports = {
 
-  scoreVideo: (videos, endpoints, requests, x = 1, y = 1, z = 1) => {
+  scoreVideo: (endpoints, videos, x = 1, y = 1, z = 1) => {
 
     _.each(videos, video => {
 
-      video.score = 0
+      video.score = 0;
     });
 
-    _.each(requests, request => {
-
-      const video = videos[request.videoId];
-      const endpoint = endpoints[request.endpointId];
-
-      video.score += x * request.numberOfRequests / y * video.size * z * endpoint.latency;
+    _.each(endpoints, endpoint => {
+      _.each(endpoint.requests, request => {
+        const video = videos[request.videoId];
+        video.score += x * request.numberOfRequests / y * video.size * z * endpoint.latency;
+      });
     });
 
     return _.sortBy(videos, video => -video.score);
--- test/score-video-test.js
+++ test/score-video-test.js
@@ -11,28 +11,15 @@ describe('Score videos', () => {
     // GIVEN
 
     // WHEN
-    const scoredVideos = ScoreVideo.scoreVideo([
-        {id: 0, score: 9, size: 1},
-        {id: 1, score: 8, size: 1}
-      ],
-      [{
-        latency: 1
-      }],
-      [{
-        videoId: 0,
-        endpointId: 0,
-        numberOfRequests: 1
-      },
-        {
-          videoId: 1,
-          endpointId: 0,
-          numberOfRequests: 2
-        }]);
+    const scoredVideos = ScoreVideo.scoreVideo(
+      [{latency: 1, requests: [{numberOfRequests: 1, videoId: 0}, {numberOfRequests: 2, videoId: 1}]}],
+      [{id: 0, score: 9, size: 1}, {id: 1, score: 8, size: 1}]
+    );
 
     // THEN
     assert.deepEqual([
       {id: 1, score: 2, size: 1},
-      {id: 0, score: 1, size: 1}
+      {id: 0, score: 1, size: 1},
     ], scoredVideos);
   });
 
