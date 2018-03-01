082909e3ebf9a8bd39c2bcdcd35c2925ef5b627e
jsmadja <julien.smadja@gmail.com>
2018-03-01 19:24:39 +0100

    Add a_example to index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const Files = require('./utils/files');
 const Main = require('./main');
 const Output = require('./models/output');
 
-FILE = 'example';
-const [data] = Files.fileReader(`./data/${FILE}.in`);
+FILE = 'a_example';
+const algoInput = Files.fileReader(`./data/${FILE}.in`);
 
-const solution = Main.calcSolution(data);
+const solution = Main.calcSolution(algoInput);
 
 Output.writeFile(('./' + FILE + '.out'), Output.write(solution));
