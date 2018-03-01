14d30a6e1654a56c9a9acaa120f7421418c925d3
jsmadja <julien.smadja@gmail.com>
2018-03-01 19:23:40 +0100

    Parsing a_example.in

--- src/utils/files.js
+++ src/utils/files.js
@@ -6,9 +6,37 @@ module.exports = {
   fileReader: filePath => {
     const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
     let splittedContent = fileContent.trim().split('\n');
-    return _(splittedContent).map(row => row.split(' '))
-    .map(row => ({ R: row[0], C: row[1], F: row[2], N: row[3], B: row[4], T: row[5] }))
-    .value();
+    let firstLine = splittedContent[0];
+    const dataFirstLine = _.map(firstLine.split(' '), d => parseInt(d));
+    const algoInput = {
+      grid: {
+        x: dataFirstLine[0],
+        y: dataFirstLine[1]
+      },
+      vehicles: dataFirstLine[2],
+      bonus: dataFirstLine[4],
+      steps: dataFirstLine[5],
+      rides: [],
+    };
+    for (let i = 1; i <= dataFirstLine[3]; i++) {
+      let rideLine = splittedContent[i];
+      const rideFirstLine = _.map(rideLine.split(' '), d => parseInt(d));
+      algoInput.rides.push(
+        {
+          start: {
+            x: rideFirstLine[0],
+            y: rideFirstLine[1],
+            time: rideFirstLine[4],
+          },
+          finish: {
+            x: rideFirstLine[2],
+            y: rideFirstLine[3],
+            time: rideFirstLine[5],
+          },
+        }
+      );
+    }
+    return algoInput;
   },
 
 };
--- test/files-test.js
+++ test/files-test.js
@@ -5,7 +5,54 @@ describe('Files', () => {
 
   it('should parse a_example', () => {
     const data = Files.fileReader('./data/a_example.in');
-    console.log(data);
+    assert.deepEqual(data,
+      {
+        grid: {
+          x: 3,
+          y: 4,
+        },
+        vehicles: 2,
+        bonus: 2,
+        steps: 10,
+        rides: [
+          {
+            start: {
+              x: 0,
+              y: 0,
+              time: 2,
+            },
+            finish: {
+              x: 1,
+              y: 3,
+              time: 9,
+            }
+          },
+          {
+            start: {
+              x: 1,
+              y: 2,
+              time: 0,
+            },
+            finish: {
+              x: 1,
+              y: 0,
+              time: 9,
+            }
+          },
+          {
+            start: {
+              x: 2,
+              y: 0,
+              time: 0,
+            },
+            finish: {
+              x: 2,
+              y: 2,
+              time: 9,
+            }
+          }
+        ]
+      });
   });
 
 });
